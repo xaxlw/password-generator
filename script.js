ab1aaa9ebe7330ffb812d28fe3608fe5166a6715
awlx <ayuxkaic@gmail.com>
2023-07-07 14:10:57 -0500

    random gen passw

---
+++ script.js
@@ -0,0 +1,19 @@
+let password = document.getElementById('password');
+
+function genpass() {
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let passwordLength = 12;
+    let password = " ";
+    
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
+}
+
+function copy() {
+    let copytext = document.getElementById('password');
+    copytext.select();
+    document.execCommand("copy");
+}
